758c6cd03bff3738ecd0cbc527fd6afb37134de4
AstralWanderer <renshi0226@gmail.com>
2023-11-10 16:35:19 +0800

    KRA_2 retrieves from database

--- KRA_2.php
+++ KRA_2.php
@@ -1,350 +1,395 @@
+<?php
+
+require 'functions.php';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <link rel="stylesheet" href="/CSS/KRA.css">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="shortcut icon" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmPodt5vh1lKwmlGvOSj3xeXqUp_TokegUQV_5WxVqgw&s">
-  <title>KRA II</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/CSS/KRA.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon"
+          href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmPodt5vh1lKwmlGvOSj3xeXqUp_TokegUQV_5WxVqgw&s">
+    <title>KRA II</title>
 </head>
-
 <style>
-  @font-face{
-  font-family: Inter;
-  src: url(./fonts/Inter-VariableFont_slnt\,wght.ttf);
-}
+    @font-face {
+        font-family: Inter;
+        src: url(./fonts/Inter-VariableFont_slnt\,wght.ttf);
+    }
 
 
-body{
-  background: url('./Assets/KRA_files_bg.jpg');
-  background-size: cover;
-}
+    body {
+        background: url('./Assets/KRA_files_bg.jpg');
+        background-size: cover;
+    }
 
-* {
-  margin: 0;
-  padding: 0;
-  font-family: Inter;
-  text-decoration: none;
-  align-items:  center;
-}
+    * {
+        margin: 0;
+        padding: 0;
+        font-family: Inter;
+        text-decoration: none;
+        align-items: center;
+    }
 
 
-td{
- background-color:#dbd7d7;
-}
+    td {
+        background-color: #dbd7d7;
+    }
 
-input{
-  text-align: center;
-}
+    input {
+        text-align: center;
+    }
 
-.title{
-  text-align: center;
-  font-size: 50px;
-}
+    .title {
+        text-align: center;
+        font-size: 50px;
+    }
 
-.row__des td{
-  padding: 10px;
-  color: white;
-  background-color: #7d0201;
-  text-align: center;
-}
+    .row__des td {
+        padding: 10px;
+        color: white;
+        background-color: #7d0201;
+        text-align: center;
+    }
 
-.hidden{
-  background-color: none;
-}
+    .hidden {
+        background-color: none;
+    }
 
-.form__btns{
-  text-align: center;
-}
+    .form__btns {
+        text-align: center;
+    }
 
-.form__btns button{
-  align-items: center;
-  color: white;
-  background-color: #7d0201;
-  border: solid #afd9ea;
-  border-radius: 15px;
-  padding: 5px;
-  font-size: 20px;
-}
+    .form__btns button {
+        align-items: center;
+        color: white;
+        background-color: #7d0201;
+        border: solid #afd9ea;
+        border-radius: 15px;
+        padding: 5px;
+        font-size: 20px;
+    }
 
-table {
-  font-size: 20px;
-  margin-left: auto;
-  margin-right: auto;
-   min-width: 1000px;
-}
+    table {
+        font-size: 20px;
+        margin-left: auto;
+        margin-right: auto;
+        min-width: 1000px;
+    }
 
 </style>
 
 <body>
 
-  <h1 class="title">KRA II</h1>
+<?php
+
+$query = "SELECT * FROM kra_2 WHERE KRA2_ID = 1";
+$result = mysqli_query($conn, $query);
+while ($row = mysqli_fetch_array($result)) {
+    ?>
+
+    <h1 class="title">KRA II</h1>
 
     <br><br>
-  <table>
+    <br><br><br>
 
-    <tr class="row__des">
-      <td colspan="3"><h2>CRITERION A - RESEARCH OUTPUTS (MAX = 100 POINTS)</h2></td>
-    </tr>
+    <?php
+    $soleAuthorship = $row['Crit_A_SA'];
+    $coAuthorship = $row['Crit_A_CA'];
+    $total_1 = $soleAuthorship + $coAuthorship;
 
-    <tr><br></tr>
+    $leadResearcher = $row['Crit_A_LR'];
+    $contributor = $row['Crit_A_Contrib'];
+    $total_2 = $leadResearcher + $contributor;
 
-    <tr class="row__des">
-      <td>SCHOLARLY RESEARCH PAPERS PUBLISHED</td>
-      <td>Points</td>
-      <td>Allowable Points</td>
-    </tr>
-    
-   
+    $local = $row['Crit_A_Local'];
+    $international = $row['Crit_A_International'];
+    $total_3 = $local + $international;
 
-    <tr>
-      <td>Sole Authorship</td>
-      <td><input type="text" disabled></td>
-      <td></td>
-    </tr>
+    $total_A = $total_1 + $total_2 + $total_3;
+    ?>
 
-    <tr>
-      <td>Co-Authorship</td>
-      <td><input type="text" disabled></td>
-      <td></td>
-    </tr>
+    <table>
 
-    <tr>
-      <td><b>TOTAL POINTS: </b></td>
-      <td><input disabled type="text"></td>
-      <td></td>
-    </tr>
+        <tr class="row__des">
+            <td colspan="3"><h2>CRITERION A - RESEARCH OUTPUTS (MAX = 100 POINTS)</h2></td>
+        </tr>
 
-    <tr class="row__des">
-      <td>RESEARCH OUTPUT TRANSLATED INTO PROJECT, POLICY OR PRODUCT</td>
-      <td></td>
-      <td></td>
-    </tr>
+        <tr><br></tr>
 
-    <tr>
-      <td>Lead Researcher</td>
-      <td><input type="text" disabled></td>
-      <td></td>
-    </tr>
+        <tr class="row__des">
+            <td>SCHOLARLY RESEARCH PAPERS PUBLISHED</td>
+            <td>Points</td>
+            <td>Allowable Points</td>
+        </tr>
 
-    <tr>
-      <td>Contributor</td>
-      <td><input type="text" disabled></td>
-      <td></td>
-    </tr>
 
-    <tr>
-      <td><b>TOTAL POINTS: </b></td>
-      <td><input disabled type="text"></td>
-      <td></td>
-    </tr>
+        <tr>
+            <td>Sole Authorship</td>
+            <td><input type="text" value="<?= $soleAuthorship ?>" disabled></td>
+            <td></td>
+        </tr>
 
-    <tr class="row__des">
-      <td>RESEARCH PUBLICATION CITED</td>
-      <td colspan="2"></td>
-    </tr>
+        <tr>
+            <td>Co-Authorship</td>
+            <td><input type="text" value="<?= $coAuthorship ?>" disabled></td>
+            <td></td>
+        </tr>
 
-    <tr>
-      <td>Local (MAX = 40 POINTS)</td>
-      <td><input type="text" disabled></td>
-      <TD><input type="text" disabled></TD>
-    </tr>
+        <tr>
+            <td><b>TOTAL POINTS: </b></td>
+            <td><input disabled value="<?= $total_1 ?>" type="text"></td>
+            <td></td>
+        </tr>
 
-    <tr>
-      <td>International (MAX = 60 POINTS)</td>
-      <td><input type="text" disabled></td>
-      <td><input type="text" disabled></td>
-    </tr>
+        <tr class="row__des">
+            <td>RESEARCH OUTPUT TRANSLATED INTO PROJECT, POLICY OR PRODUCT</td>
+            <td></td>
+            <td></td>
+        </tr>
 
-    <tr>
-      <td><b>TOTAL POINTS: </b></td>
-      <td><input disabled type="text"></td>
-      <td></td>
-    </tr>
+        <tr>
+            <td>Lead Researcher</td>
+            <td><input type="text" value="<?= $leadResearcher ?>" disabled></td>
+            <td></td>
+        </tr>
 
-    <tr>
-      <td><b>TOTAL POINTS FOR CRITERION A: </b></td>
-      <td><input type="text" disabled></td>
-      <td><input type="text" disabled></td>
-    </tr>
-  </table>
+        <tr>
+            <td>Contributor</td>
+            <td><input type="text" value="<?= $contributor ?> " disabled></td>
+            <td></td>
+        </tr>
 
-  <br><br><br>
-  <table> 
-    <tr class="row__des">
-      <td colspan="3"><h2>CRITERION B - INVENTIONS (MAX = 100 POINTS)</h2></td>
-    </tr>
+        <tr>
+            <td><b>TOTAL POINTS: </b></td>
+            <td><input value="<?= $total_2 ?>" disabled type="text"></td>
+            <td></td>
+        </tr>
 
-    <tr class="row__des">
-      <td>PATENTED INVENTIONS</td>
-      <td>Points</td>
-      <td>Allowable Points</td>
-    </tr>
+        <tr class="row__des">
+            <td>RESEARCH PUBLICATION CITED</td>
+            <td colspan="2"></td>
+        </tr>
 
-    <tr class="row__des">
-      <td>Patentable Inventions</td>
-      <td colspan="2"></td>
-    </tr>
+        <tr>
+            <td>Local (MAX = 40 POINTS)</td>
+            <td><input type="text" value="<?= $local ?>" disabled></td>
+            <TD><input type="text" value="<?= $local ?>" disabled></TD>
+        </tr>
 
-    <!-- <tr class="row__des">
-      <td>Invention Patient</td>
-      <td></td>
-      <td></td>
-    </tr> -->
+        <tr>
+            <td>International (MAX = 60 POINTS)</td>
+            <td><input type="text" value="<?= $international ?>" disabled></td>
+            <td><input type="text" value="<?= $international ?>" disabled></td>
+        </tr>
 
-    <tr>
-      <td>- Sole Inventor</td>
-      <td><input disabled type="text"></td>
-      <td><input disabled type="text"></td>
-    </tr>
-    <tr>
-      <td>- Co-Inventor</td>
-      <td><input disabled type="text"></td>
-      <td><input disabled type="text"></td>
-    </tr>
-    <tr class="row__des">
-      <td>Utility Model and Industrial Design</td>
-      <td colspan="2"></td>
-    </tr>
-    <tr>
-      <td>- Sole Inventor</td>
-      <td><input disabled type="text"></td>
-      <td><input disabled type="text"></td>
-    </tr>
-    <tr>
-      <td>- Co-Inventor</td>
-      <td><input disabled type="text"></td>
-      <td><input disabled type="text"></td>
-    </tr>
-    <tr class="row__des">
-      <td>Commercialized Patented Product</td>
-      <td colspan="2"></td>
-    </tr>
-    <!-- <tr>
-      <td>Commercialized Patented Product</td>
-      <td><input disabled type="text"></td>
-      <td></td>
-    </tr> -->
-    <tr>
-      <td>Local (MAX = 20 POINTS)</td>
-      <td><input disabled type="text"></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td>International (MAX = 30 POINTS)</td>
-      <td><input disabled type="text"></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td><b>TOTAL POINTS: </b></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr class="row__des">
-      <td>NON-PATENTABLE INVENTIONS</td>
-      <td colspan="2"></td>
-    </tr>
-    <tr>
-      <td>Copyrighted and Utilized Software Products </td>
-      <td colspan="2"></td>
-    </tr>
+        <tr>
+            <td><b>TOTAL POINTS: </b></td>
+            <td><input value="<?= $total_3 ?>" disabled type="text"></td>
+            <td></td>
+        </tr>
 
-    <tr>
-      <td>New Software Products</td>
-      <td></td>
-      <td></td>
-    </tr>
+        <tr>
+            <td><b>TOTAL POINTS FOR CRITERION A: </b></td>
+            <td><input type="text" value="<?= $total_A ?>" disabled></td>
+            <td><input type="text" value="<?= $total_A ?>" disabled></td>
+        </tr>
+    </table>
+    <table>
+        <tr class="row__des">
+            <td colspan="3"><h2>CRITERION B - INVENTIONS (MAX = 100 POINTS)</h2></td>
+        </tr>
 
-    <tr>
-      <td>- Sole Inventor</td>  
-      <td><input disabled type="text"></td>
-      <td><input disabled type="text"></td>
-    </tr>
+        <?php
 
-    <tr>
-      <td>- Co-Inventor</td>
-      <td><input disabled type="text"></td>
-      <td><input disabled type="text"></td>
-    </tr>
+        $PI_SI = $row['Crit_B_PI_SI'];
+        $PI_CI = $row['Crit_B_PI_CI'];
+        $UMID_SI = $row['Crit_B_UMID_SI'];
+        $UMID_CI = $row['Crit_B_UMID_CI'];
+        $CPP_L = $row['Crit_B_CPP_Local'];
+        $CPP_I = $row['Crit_B_CPP_International'];
+        $NPI_SI = $row['Crit_B_NPI_SI'];
+        $NPI_CI = $row['Crit_B_NPI_CI'];
+        $NPI_USP = $row['Crit_B_NPI_USP'];
+        $NPVA_SD = $row['Crit_B_NPVA_SD'];
+        $NPVA_CD = $row['Crit_B_NPVA_CD'];
+        $total_B = $row['Crit_B_Total'];
+        $PA = $row['Crit_C_PA'];
+        $Exhib = $row['Crit_C_Exhibition'];
+        $Designs = $row['Crit_C_Designs'];
+        $Lit_pub = $row['Crit_C_Lit-pub'];
+        $total_C = $row['Crit_C_total'];
+        $totalKRA2 = $row['KRA_2_total'];
+        ?>
 
-    <tr>
-      <td>Updated Software Products</td>
-      <td><input disabled type="text"></td>
-      <td></td>
-    </tr>
+        <tr class="row__des">
+            <td>PATENTED INVENTIONS</td>
+            <td>Points</td>
+            <td>Allowable Points</td>
+        </tr>
 
-    <tr class="row__des">
-      <td>New Plant Variety, Animal Breed, New Microbial Strain</td>
-      <td colspan="2"></td>
-    </tr>
+        <tr class="row__des">
+            <td>Patentable Inventions</td>
+            <td colspan="2"></td>
+        </tr>
 
-    <tr>
-      <td>Sole Developer</td>
-      <td><input disabled type="text"></td>
-      <td></td>
-    </tr>
+        <!-- <tr class="row__des">
+          <td>Invention Patient</td>
+          <td></td>
+          <td></td>
+        </tr> -->
 
-    <tr>
-      <td>Co-Developer</td>
-      <td><input disabled type="text"></td>
-      <td></td>
-    </tr>
-    
-    <tr>
-      <td><b>TOTAL POINTS FOR CRITERION B: </b></td>
-      <td><input disabled type="text"></td>
-      <td><input disabled type="text"></td>
-    </tr>
-  </table>
+        <tr>
+            <td>- Sole Inventor</td>
+            <td><input value="<?= $PI_SI ?>" disabled type="text"></td>
+            <td><input value="<?= $PI_SI ?>" disabled type="text"></td>
+        </tr>
+        <tr>
+            <td>- Co-Inventor</td>
+            <td><input value="<?= $PI_CI ?>" disabled type="text"></td>
+            <td><input value="<?= $PI_CI ?>" disabled type="text"></td>
+        </tr>
+        <tr class="row__des">
+            <td>Utility Model and Industrial Design</td>
+            <td colspan="2"></td>
+        </tr>
+        <tr>
+            <td>- Sole Inventor</td>
+            <td><input value="<?= $UMID_SI ?>" disabled type="text"></td>
+            <td><input value="<?= $UMID_SI ?>" disabled type="text"></td>
+        </tr>
+        <tr>
+            <td>- Co-Inventor</td>
+            <td><input value="<?= $UMID_CI ?>" disabled type="text"></td>
+            <td><input value="<?= $UMID_CI ?>" disabled type="text"></td>
+        </tr>
+        <tr class="row__des">
+            <td>Commercialized Patented Product</td>
+            <td colspan="2"></td>
+        </tr>
+        <!-- <tr>
+          <td>Commercialized Patented Product</td>
+          <td><input disabled type="text"></td>
+          <td></td>
+        </tr> -->
+        <tr>
+            <td>Local (MAX = 20 POINTS)</td>
+            <td><input value="<?= $CPP_L ?>" disabled type="text"></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>International (MAX = 30 POINTS)</td>
+            <td><input value="<?= $CPP_I ?>" disabled type="text"></td>
+            <td></td>
+        </tr>
+        <tr class="row__des">
+            <td>NON-PATENTABLE INVENTIONS</td>
+            <td colspan="2"></td>
+        </tr>
+        <tr>
+            <td>Copyrighted and Utilized Software Products</td>
+            <td colspan="2"></td>
+        </tr>
 
-  <br><br><br>
+        <tr>
+            <td>New Software Products</td>
+            <td></td>
+            <td></td>
+        </tr>
 
-  <table>
-    <tr class="row__des">
-      <td colspan="3"><h2>CRITERION C - CREATIVE WORKS (MAX = 100 POINTS)</h2></td>
-    </tr>
-    <tr class="row__des">
-      <td>CREATIVE WORKS PERFORMED, EXHIBITED, DESIGNED & PUBLISHED</td>
-      <td>Points</td>
-      <td>Allowable Points</td>
-    </tr>
+        <tr>
+            <td>- Sole Inventor</td>
+            <td><input value="<?= $NPI_SI?>" disabled type="text"></td>
+            <td><input value="<?= $NPI_SI?>" disabled type="text"></td>
+        </tr>
 
-    <tr>
-      <td>Performing Arts</td>
-      <td><input disabled type="text"></td>
-      <td></td>
-    </tr>
+        <tr>
+            <td>- Co-Inventor</td>
+            <td><input value="<?= $NPI_CI?>" disabled type="text"></td>
+            <td><input value="<?= $NPI_CI?>" disabled type="text"></td>
+        </tr>
 
-    <tr>
-      <td>Exhibitions</td>
-      <td><input disabled type="text"></td>
-      <td></td>
-    </tr>
+        <tr>
+            <td>Updated Software Products</td>
+            <td><input value="<?= $NPI_USP?>" disabled type="text"></td>
+            <td></td>
+        </tr>
 
-    <tr>
-      <td>Designs</td>
-      <td><input disabled type="text"></td>
-      <td></td>
-    </tr>
+        <tr class="row__des">
+            <td>New Plant Variety, Animal Breed, New Microbial Strain</td>
+            <td colspan="2"></td>
+        </tr>
 
-    <tr>
-      <td>Literary Publication</td>
-      <td><input disabled type="text"></td>
-      <td></td>
-    </tr>
+        <tr>
+            <td>Sole Developer</td>
+            <td><input value="<?= $NPVA_SD ?>" disabled type="text"></td>
+            <td></td>
+        </tr>
 
-    <tr>
-      <td><b>TOTAL POINTS FOR CRITERION C (MAX - 100 points)</b></td>
-      <td><input disabled type="text"></td>
-      <td><input disabled type="text"></td>
-    </tr>
-    
-    <tr>
-      <td><b><h2>GRAND TOTAL POINTS FOR KRA II (MAX - 100 points)</h2></b></td>
-      <td><input disabled type="text"></td>
-      <td><input disabled type="text"></td>
-    </tr>
-  </table>
+        <tr>
+            <td>Co-Developer</td>
+            <td><input value="<?= $NPVA_CD ?>" disabled type="text"></td>
+            <td></td>
+        </tr>
 
+        <tr>
+            <td><b>TOTAL POINTS FOR CRITERION B: </b></td>
+            <td><input value="<?= $total_B ?>" disabled type="text"></td>
+            <td><input value="<?= $total_B ?>" disabled type="text"></td>
+        </tr>
+    </table>
+
+    <br><br><br>
+
+    <table>
+        <tr class="row__des">
+            <td colspan="3"><h2>CRITERION C - CREATIVE WORKS (MAX = 100 POINTS)</h2></td>
+        </tr>
+        <tr class="row__des">
+            <td>CREATIVE WORKS PERFORMED, EXHIBITED, DESIGNED & PUBLISHED</td>
+            <td>Points</td>
+            <td>Allowable Points</td>
+        </tr>
+
+        <tr>
+            <td>Performing Arts</td>
+            <td><input value="<?= $PA ?>" disabled type="text"></td>
+            <td></td>
+        </tr>
+
+        <tr>
+            <td>Exhibitions</td>
+            <td><input value="<?= $Exhib ?>" disabled type="text"></td>
+            <td></td>
+        </tr>
+
+        <tr>
+            <td>Designs</td>
+            <td><input value="<?= $Designs ?>" disabled type="text"></td>
+            <td></td>
+        </tr>
+
+        <tr>
+            <td>Literary Publication</td>
+            <td><input value="<?= $Lit_pub ?>" disabled type="text"></td>
+            <td></td>
+        </tr>
+
+        <tr>
+            <td><b>TOTAL POINTS FOR CRITERION C (MAX - 100 points)</b></td>
+            <td><input value="<?= $total_C ?>" disabled type="text"></td>
+            <td><input value="<?= $total_C ?>" disabled type="text"></td>
+        </tr>
+
+        <tr>
+            <td><b><h2>GRAND TOTAL POINTS FOR KRA II (MAX - 100 points)</h2></b></td>
+            <td><input value="<?= $totalKRA2 ?>" disabled type="text"></td>
+            <td><input value="<?= $totalKRA2 ?>" disabled type="text"></td>
+        </tr>
+    </table>
+<?php } ?>
 </body>
 </html>
