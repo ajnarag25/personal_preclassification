dc1b3d6c599a9ca30f764515690f2bf469b26200
aj <ajnarag25@gmail.com>
2023-11-24 00:46:59 +0800

    change label

--- registration.php
+++ registration.php
@@ -13,7 +13,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="assets/evsu-logo-removebg-preview.png" rel="icon">
-    <title>Personal Preclassification - Register</title>
+    <title>Personal Preclassification - Sign up</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Anton&family=Inter:wght@700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css" rel="stylesheet">
